e33e9615bb13ca81eb8b1a62e8977f085b34a32e
Oscar Hernandez <oscarhermar01@gmail.com>
2023-10-29 14:49:05 -0600

    Initial commit

---
+++ app/src/main/java/com/example/powercalc_1/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.powercalc_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main) //Esta linea esta ligada al xml en layout
+    }
+}
